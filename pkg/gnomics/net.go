9603e6d8229a0d3c02efc7caed72128d7ff66209
Serenity <d.palbanese@gmail.com>
2021-02-13 18:16:43 +0100

    Added authentication helpers

--- pkg/gnomics/net.go
+++ pkg/gnomics/net.go
@@ -10,14 +10,49 @@ import (
 
 var client = http.Client{}
 
+func prepareQueryForAuth(q *QueryParams) error {
+	if *q == nil {
+		*q = make(map[string]string)
+	}
+	if err := InjectApiKey(*q); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Will execute a get-request, inject the api key if it exists and then parse the data.
+func getRequestParsed(endpoint string, queryParams QueryParams, data *interface{}) error {
+	if err := prepareQueryForAuth(&queryParams); err != nil {
+		return err
+	}
+
+	resp, err := request(endpoint, http.MethodGet, queryParams, nil)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(resp, data)
+	return err
+}
+
+// Will execute a getRequest and inject the api key if it exists
 func getRequest(endpoint string, queryParams QueryParams) ([]byte, error) {
+	if err := prepareQueryForAuth(&queryParams); err != nil {
+		return nil, err
+	}
+
 	return request(endpoint, http.MethodGet, queryParams, nil)
 }
 
+// Will execute a postRequest and inject the api key if it exists
 func postRequest(endpoint string, queryParams QueryParams, body interface{}) ([]byte, error) {
+	if err := prepareQueryForAuth(&queryParams); err != nil {
+		return nil, err
+	}
+
 	return request(endpoint, http.MethodPost, queryParams, body)
 }
 
+// General request function. This just executes and passes the errors or data.
 func request(endpoint string, httpMethod string, queryParams QueryParams, body interface{}) ([]byte, error) {
 	url := endpoint
 	if queryParams != nil {
